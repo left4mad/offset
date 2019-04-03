cf2663f7bcc77dd3708d2c9800a98206583c624f
left4mad <edleft4mad@gmail.com>
2019-04-03 10:18:46 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import html from "./template/pages/index.pug"
 // Import styles
 import style from "./styles/style.scss";
 // Import scripts
+import jquery from "./scripts/jquery.min.js";
+import popper from "./scripts/popper.min.js";
+import bootstrap from "./scripts/bootstrap.min.js";
 import script from "./scripts/main.js";
